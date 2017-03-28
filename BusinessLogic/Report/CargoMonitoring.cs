f69beffcb024a27e7a145792754a6bd52cdf765f
msantos-microbiz <mark.santos@microbizone.com>
2017-03-28 14:26:57 +0800

    Updates in Reports Cargo Monitoring FLM 3/28

---
+++ BusinessLogic/Report/CargoMonitoring.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL = DataAccess;
+
+namespace BusinessLogic.Report
+{
+    public class CargoMonitoring
+    {
+        public static DataSet GetCargoMonitoringDelivered(string conSTR, DateTime? date1, DateTime? date2)
+        {
+            return DAL.Reports.CargoMonitoring.GetCargoMonitoringDelivered(conSTR, date1, date2);
+        }
+
+        public static DataSet GetCargoMonitoringHold(string conSTR, DateTime? date1, DateTime? date2)
+        {
+            return DAL.Reports.CargoMonitoring.GetCargoMonitoringHold(conSTR, date1, date2);
+        }
+    }
+}
